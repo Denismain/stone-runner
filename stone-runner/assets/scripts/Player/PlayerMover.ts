69ee1b924c288e0a7e6d7888b1fc079e101ecca0
Denismain <97342334+Denismain@users.noreply.github.com>
2026-03-17 11:55:42 +0300

    player jump test

--- stone-runner/assets/scripts/Player/PlayerMover.ts
+++ stone-runner/assets/scripts/Player/PlayerMover.ts
@@ -1,16 +1,21 @@
-import { _decorator, CCFloat, Component, Node } from 'cc';
-const { ccclass, property } = _decorator;
+import {_decorator, CCFloat, Component, RigidBody, v3} from 'cc';
+const {ccclass, property} = _decorator;
 
 @ccclass('PlayerMover')
 export class PlayerMover extends Component {
     @property(CCFloat) playerSpeed: number = 50;
+    @property(CCFloat) jumpForce: number = 4;
 
-    start() {
+    @property(RigidBody) private playerRigidBody: RigidBody = null;
 
+    protected start(): void {
+        this.scheduleOnce(() => {
+            this.executeJump();
+        }, 3);
     }
 
-    update(deltaTime: number) {
-        
+    private executeJump(): void {
+        this.playerRigidBody.applyImpulse(v3(0, this.jumpForce, 0));
     }
 }
 
