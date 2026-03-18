617d4cca0434aecda5bd270fb13e5e042e908145
Denismain <97342334+Denismain@users.noreply.github.com>
2026-03-18 20:31:28 +0300

    UI logic

---
+++ stone-runner/assets/scripts/UIController.ts
@@ -0,0 +1,90 @@
+import {_decorator, CCFloat, Component, Enum, Node, tween, UIOpacity, v3} from 'cc';
+import EventManager from './Plugins/EventManager';
+import Events from './Enums/Events';
+import Easings from './Enums/Easings';
+const {ccclass, property} = _decorator;
+
+@ccclass('UIController')
+export class UIController extends Component {
+    @property(Node) private gameLogoNode: Node = null;
+    @property(Node) private installButtonNode: Node = null;
+    @property({type: Enum(Easings)}) private hideEasingType: Easings = Easings.backIn;
+    @property(CCFloat) private hideDelay: number = 0.3;
+
+    @property(UIOpacity) private shadowOpacity: UIOpacity = null;
+    @property({type: Enum(Easings)}) private shadowEasingType: Easings = Easings.sineInOut;
+    @property(CCFloat) private shadowShowDelay: number = 0.2;
+
+    @property(Node) private endScreenLogoNode: Node = null;
+    @property({type: Enum(Easings)}) private endScreenEasingType: Easings = Easings.backOut;
+    @property(CCFloat) private endScreenShowDelay: number = 0.3;
+
+    protected onEnable(): void {
+        EventManager.on(Events.GAMEPLAY_END, this.onGameplayEnd, this);  
+    }
+
+    protected onDisable(): void {
+        EventManager.on(Events.GAMEPLAY_END, this.onGameplayEnd, this);
+    }
+
+    private async hideUI(): Promise<void> {
+        this.executeTweenHide(this.gameLogoNode);
+
+        await this.executeTweenHide(this.installButtonNode);
+    }
+
+    private async executeTweenHide(node: Node): Promise<void> {
+        const easingType = this.hideEasingType as any;
+
+        return new Promise<void>((resolve) => {
+            tween(node)
+                .to(this.hideDelay, 
+                    {scale: v3}, 
+                    {easing: easingType}
+                )
+                .call(() => {
+                    resolve();
+                })
+                .start();
+        });
+    }
+
+    private async showEndScreen(): Promise<void> {
+        await this.showShadow();
+
+        this.endLogoShow();
+    }
+
+    private async showShadow(): Promise<void> {
+        const easingType = this.shadowEasingType as any;
+
+        return new Promise<void>((resolve) => {
+            tween(this.shadowOpacity)
+                .to(this.shadowShowDelay, 
+                    {opacity: 255}, 
+                    {easing: easingType}
+                )
+                .call(() => {
+                    resolve();
+                })
+                .start();
+        });
+    }
+
+    private endLogoShow(): void {
+        const easingType = this.endScreenEasingType as any;
+
+        tween(this.endScreenLogoNode)
+            .to(this.endScreenShowDelay,
+                {scale: v3(1, 1, 1)},
+                {easing: easingType}
+            )
+            .start();
+    }
+
+    private async onGameplayEnd(): Promise<void> {
+        await this.hideUI();
+
+        this.showEndScreen();
+    }
+}
