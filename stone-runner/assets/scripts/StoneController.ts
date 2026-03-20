91b923d82cd457e755181b4fa43b782a6d6495ea
Denismain <97342334+Denismain@users.noreply.github.com>
2026-03-20 17:59:48 +0300

    restart logic fix

--- stone-runner/assets/scripts/StoneController.ts
+++ stone-runner/assets/scripts/StoneController.ts
@@ -8,6 +8,7 @@ const {ccclass, property} = _decorator;
 @ccclass('StoneController')
 export class StoneController extends Component {
     @property(Node) private stonesParent: Node = null;
+    @property(Node) private playerNode: Node = null;
 
     @property(CCFloat) private stoneYOffset: number = 0.4;
     @property(CCInteger) private stoneCount: number = 5;
@@ -16,7 +17,8 @@ export class StoneController extends Component {
 
     @property(Vec3) private spawnAngle: Vec3 = new Vec3();
 
-    private offset: Vec3 = new Vec3();
+    private stonesOffset: Vec3 = new Vec3();
+    private playerStonesOffset: number = 0;
     
     private poolManager: PoolManager = null;
 
@@ -24,6 +26,8 @@ export class StoneController extends Component {
 
     protected onLoad(): void {
         this.poolManager = PoolManager.getInstance();
+
+        this.playerStonesOffset = this.calucateOffset();
     }
 
     protected onEnable(): void {
@@ -38,6 +42,15 @@ export class StoneController extends Component {
         this.spawnStones();
     }
 
+    private calucateOffset(): number {
+        const stonesParentWorldPos = this.stonesParent.worldPosition;
+        const playerWorldPos = this.playerNode.worldPosition;
+
+        const zOffset = stonesParentWorldPos.z - playerWorldPos.z;
+
+        return zOffset;
+    }
+
     private spawnStones(): void {
         for (let i: number = 0; i < this.stoneCount; i++) {
             this.createStone();
@@ -45,13 +58,13 @@ export class StoneController extends Component {
     }
 
     private createStone(): void {
-        const stone = this.poolManager.getObject(this.offset, this.stonesParent);
+        const stone = this.poolManager.getObject(this.stonesOffset, this.stonesParent);
          
         stone.node.y = this.stoneYOffset;
         stone.node.setRotationFromEuler(this.spawnAngle);
 
         const randomOffset = this.minZOffset + Math.random() * (this.maxZOffset - this.minZOffset);
-        this.offset.z += randomOffset;        
+        this.stonesOffset.z += randomOffset;        
 
         this.stonesArray.push(stone);
     }
@@ -68,6 +81,14 @@ export class StoneController extends Component {
     }
 
     private onRestart(): void {
+        this.stonesOffset.z = 0;
+
+        this.stonesParent.setPosition(
+            this.stonesParent.x,
+            this.stonesParent.y,
+            this.playerNode.position.z + this.playerStonesOffset
+        );
+
         this.respawnStones();
     }
 }
